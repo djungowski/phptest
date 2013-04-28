a8311680f08bcb6b8afc7a9a7f28ebbed5b6f37e
Dominik Jungowski <Dominik@Jungowski.de>
2013-04-29 01:06:22 +0200

    docs for autoloader

--- src/PHPTest/Autoload.php
+++ src/PHPTest/Autoload.php
@@ -3,12 +3,22 @@ namespace PHPTest;
 
 class Autoload
 {
+	/**
+	 * Register autoloader
+	 *
+	 * @param String $srcPath 	The path of the PHPTest source files
+	 */
 	public function __construct($srcPath)
 	{
 		$this->addToIncludePath($srcPath);
 		spl_autoload_register(array($this, 'load'));
 	}
 
+	/**
+	 *	Add a path to the include path
+	 *
+	 * @üaram String $path
+	 */
 	private function addToIncludePath($path)
 	{
 		$includePath = get_include_path();
@@ -16,6 +26,11 @@ class Autoload
 		set_include_path($includePath);
 	}
 
+	/**
+	 * Autoload a specific class
+	 *
+	 * @param String $className
+	 */
 	public function load($className)
 	{
 		$classFile = str_replace("\\", DIRECTORY_SEPARATOR, $className);

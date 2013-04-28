a34865c35de2472022c84326514f2f0970a14489
Dominik Jungowski <Dominik@Jungowski.de>
2013-04-29 00:06:23 +0200

    stats

--- src/PHPTest/TestCase.php
+++ src/PHPTest/TestCase.php
@@ -3,19 +3,29 @@ namespace PHPTest;
 
 class TestCase
 {
+	private $stats = array(
+		'run' => 0,
+		'pass' => 0,
+		'fail' => 0
+	);
+
 	public function run()
 	{
 		$reflection = new Reflection($this);
 		foreach ($reflection->getTestMethods() as $method) {
+			$this->stats['run']++;
 			try {
 				print '.';
 				$this->$method();
+				$this->stats['pass']++;
 			} catch (\InvalidArgumentException $e) {
 				print 'F';
+				$this->stats['fail']++;
 				throw $e;
 			}
 		}
 		print PHP_EOL;
+		print 'Tests run / passed / fail: ' . $this->stats['run'] . ' / ' . $this->stats['pass'] . ' / ' . $this->stats['fail'] . PHP_EOL;
 	}
 
 	public function assertTrue($actual)
